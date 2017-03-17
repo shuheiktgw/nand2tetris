05d6f6e5eed37ab1e158546613b767a2dc9a9af4
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-03-17 09:34:27 +0900

    implemented multiplication

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @2
+  M=0
+  @i
+  M=1
+(LOOP)
+  @i
+  D=M
+  @1
+  D=D-M
+  @END
+  D;JGT
+  @0
+  D=M
+  @2
+  M=D+M
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
